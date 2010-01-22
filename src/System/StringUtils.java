2bfd2b07a0fcfca5d106e1fae7193aaf0c4b5708
Richard Jonsson <richie@richie-laptop.(none)>
2010-01-22 20:56:19 +0100

    Added class StringUtils to deal with strings obviously

---
+++ src/System/StringUtils.java
@@ -0,0 +1,22 @@
+/*
+ * Catcher
+ *
+ * License: GPL v3
+ * Authors: richard_jonsson@hotmail.com, tommyc@lavabit.com
+ */
+package System;
+
+/**
+ * Random string utilities
+ */
+public class StringUtils {
+
+    /*
+     * Replace oldStr with newStr in s
+     */
+    String replace(String s, String oldStr, String newStr) {
+        int count = s.indexOf(oldStr)-1;
+        return s.substring(0, count) + newStr +
+                s.substring(count+oldStr.length()+2);
+    }
+}
