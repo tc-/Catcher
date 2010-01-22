df376c9471dfc8eb28f71b5ee55128cdd574ca13
Tommy Carlsson <tommyc@lavabit.com>
2010-01-22 18:45:01 +0100

    Added interface for IImageLoader

--- src/System/IImageLoader.java
+++ src/System/IImageLoader.java
@@ -8,6 +8,15 @@
 package System;
 
 
+/**
+ * Loads and composes images
+ */
 public interface IImageLoader {
 
+    Object httpLoad(String url, String localCachePath, int minimumMemoryCache);
+    Object localLoad(String path, int minimumMemoryCache);
+
+    Object createImage(int width, int height);
+    Object drawImage(Object canvas, Object image, int xPos, int yPos);
+    
 }
