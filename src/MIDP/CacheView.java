b01bd46ff9825cc7c85b1f67e109d424cc6054ea
Tommy Carlsson <tommyc@lavabit.com>
2010-01-23 19:54:07 +0100

    Started with MIDPImageLoader, not working yet

--- src/MIDP/CacheView.java
+++ src/MIDP/CacheView.java
@@ -10,6 +10,7 @@ import GUI.ICacheView;
 import GUI.IViewNavigator;
 import System.Cache;
 import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.Image;
 
 public class CacheView extends CatcherCanvas implements ICacheView {
 
@@ -44,6 +45,10 @@ public class CacheView extends CatcherCanvas implements ICacheView {
 
         g.setColor(0, 0, 0);
         g.drawString("CacheView",0,40,Graphics.TOP|Graphics.LEFT);
+
+        MIDPImageLoader loader = new MIDPImageLoader();
+        Image img = (Image)loader.httpLoad("http://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png", null, 10);
+        g.drawImage(img, 0, 0, 0);
     }
     
     /**
--- src/MIDP/MIDPImageLoader.java
+++ src/MIDP/MIDPImageLoader.java
@@ -8,16 +8,35 @@
 package MIDP;
 
 import System.IImageLoader;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.microedition.io.Connector;
+import javax.microedition.lcdui.Image;
 
 
 public class MIDPImageLoader implements IImageLoader {
 
     public Object httpLoad(String url, String localCachePath, int minimumMemoryCache) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //Test image: http://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png
+        // Check if Image has been cached already
+        Object ret = localLoad(localCachePath, minimumMemoryCache);
+        
+        // Check if Image was cached
+        if (ret == null) {
+            try {
+                InputStream data = Connector.openDataInputStream(url);
+                ret = Image.createImage(data); // createImage(InputStream) is MIDP 2.0
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                ret = null;
+            }
+        }
+
+        return ret;
     }
 
     public Object localLoad(String path, int minimumMemoryCache) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return null;
     }
 
     public Object createImage(int width, int height) {
@@ -26,6 +45,11 @@ public class MIDPImageLoader implements IImageLoader {
 
     public Object drawImage(Object canvas, Object image, int xPos, int yPos) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void StoreInCache()
+    {
+
     }
 
 }
