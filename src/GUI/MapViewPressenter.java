f6175ec998970be545e70a27bc3b8dc6a7112e9b
Tommy Carlsson <tommyc@lavabit.com>
2010-01-24 13:30:40 +0100

    Added MapViewPressenter

---
+++ src/GUI/MapViewPressenter.java
@@ -0,0 +1,31 @@
+/*
+ * Catcher
+ *
+ * License: GPL v3
+ * Authors: richard_jonsson@hotmail.com, tommyc@lavabit.com
+ */
+
+package GUI;
+
+import System.ICacheProvider;
+import System.IMapProvider;
+import System.IPositionProvider;
+
+
+public class MapViewPressenter {
+
+    private final CatcherMain main;
+    private final IMapView view;
+    private final ICacheProvider cacheProvider;
+    private final IPositionProvider positionProvider;
+    private final IMapProvider mapProvider;
+
+    public MapViewPressenter(CatcherMain main, IMapView view, ICacheProvider cacheProvider, IPositionProvider positionProvider, IMapProvider mapProvider) {
+        this.main = main;
+        this.view = view;
+        this.cacheProvider = cacheProvider;
+        this.positionProvider = positionProvider;
+        this.mapProvider = mapProvider;
+    }
+
+}
